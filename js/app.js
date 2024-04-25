d4009c1953ded07e18c82c6b621411cb0056a002
TacoWarrior24 <tacowarrior24@gmail.com>
2024-04-25 14:15:15 -0400

    Formatting

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+let count = 0;
+
+function countUp() {
+    count++;
+    document.getElementById("counter").innerHTML = count;
+}
+
+//console.log('Java Script is working!');
