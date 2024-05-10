7623138118abab523cbdd134ef6a1a6731129975
TacoWarrior24 <tacowarrior24@gmail.com>
2024-05-10 14:16:11 -0400

    Added animation

--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
 // JavaScript source code
-let count = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    function fadeInOutOnScroll(elementId) {
+        var element = document.getElementById(elementId);
 
-function countUp() {
-    count++;
-    document.getElementById("counter").innerHTML = count;
-}
+        function fadeInOut() {
+            var elementTop = element.getBoundingClientRect().top;
+            var elementBottom = element.getBoundingClientRect().bottom;
+            var windowHeight = window.innerHeight;
 
-//console.log('Java Script is working!');
+            if (elementTop + 300 < windowHeight && elementBottom > 0) {
+                // Element is partially or fully visible
+                var opacity = 1 //- (elementTop + element.clientHeight) / windowHeight;
+                element.style.opacity = opacity;
+            } else {
+                // Element is outside the viewport
+                element.style.opacity = "0";
+            }
+        }
+
+        window.addEventListener('scroll', fadeInOut);
+        fadeInOut();
+    }
+
+    // Call fadeInOnScroll with the IDs of the elements you want to fade in
+    fadeInOutOnScroll('fade-in-out-aboutme');
+    fadeInOutOnScroll('fade-in-out-workhistory');
+    fadeInOutOnScroll('fade-in-out-education');
+    fadeInOutOnScroll('fade-in-out-projects');
+});
+
+console.log('Java Script is working!');
